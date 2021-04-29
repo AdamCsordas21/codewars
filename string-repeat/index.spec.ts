bb35d08259f9adbf1c9f7806aecc0aaa0749e20f
AdamCsordas21 <adamcsordas@hotmail.com>
2021-04-29 16:52:52 +0100

    Add more test cases to repeatStr function

--- string-repeat/index.spec.ts
+++ string-repeat/index.spec.ts
@@ -5,6 +5,10 @@ import {
 describe('repeatStr', () => {
   it('should repeat a given string exactly the given time', () => {
     const repeat = repeatStr(3, '*')
+    const repeat2 = repeatStr(5, '$$')
+    const repeat3 = repeatStr(6, '£££')
     expect(repeat).toEqual('***')
+    expect(repeat2).toEqual('$$$$$$$$$$')
+    expect(repeat3).toEqual('££££££££££££££££££')
   })
 })
