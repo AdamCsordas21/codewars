13141d2089958c9681106b0844d0a103f6581aea
AdamCsordas21 <adamcsordas@hotmail.com>
2021-05-05 17:39:40 +0100

    Add more test cases to reverseWords function

--- reversed-words/index.spec.ts
+++ reversed-words/index.spec.ts
@@ -5,6 +5,10 @@ import {
 describe('reversedWords', () => {
   it('should return a reversed string', () => {
     const reversed1 = reverseWords('hello world!')
+    const reverse2 = reverseWords('Manchester United')
+    const reverse3 = reverseWords('Glory Glory ManUnited!')
     expect(reversed1).toEqual('world! hello')
+    expect(reverse2).toEqual('United Manchester')
+    expect(reverse3).toEqual('ManUnited! Glory Glory')
   })
 })
