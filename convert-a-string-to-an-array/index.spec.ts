26f3bf8fac0e1ef24ea813cd61a9fe6bd5894297
AdamCsordas21 <adamcsordas@hotmail.com>
2021-05-12 18:47:38 +0100

    Add new test case to stringToArray function

--- convert-a-string-to-an-array/index.spec.ts
+++ convert-a-string-to-an-array/index.spec.ts
@@ -5,6 +5,8 @@ import {
 describe('stringToArray', () => {
   it('should convert a string to an array', () => {
     const converted = stringToArray('TypeScript')
+    const converted2 = stringToArray('TypeScript & JavaScript.')
     expect(converted).toEqual(['TypeScript'])
+    expect(converted2).toEqual(['TypeScript', '&', 'JavaScript.'])
   })
 })
