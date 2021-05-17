f462002582f560d875e6525f0371d9c85ed0fe3f
AdamCsordas21 <adamcsordas@hotmail.com>
2021-05-17 17:44:38 +0100

    Refactor calculateBMI function

--- calculate-body-mass-index/index.ts
+++ calculate-body-mass-index/index.ts
@@ -1,7 +1,4 @@
 export const calculateBMI = (weight: number, height: number): string => {
   const bmi = weight / (height * height)
-  if (bmi <= 18.0) { return 'Underweight' }
-  if (bmi <= 25.0) { return 'Normal' }
-  if (bmi <= 30.0) { return 'Overweight' }
-  else { return 'Obese' }
+  return bmi <= 18.5 ? 'Underweight' : bmi <= 25.0 ? 'Normal' : bmi <= 30.0 ? 'Overweight' : 'Obese'
 }
