e492de43bdb0dfef98bcf07a95b3bf50c978e7f3
AdamCsordas21 <adamcsordas@hotmail.com>
2021-05-19 20:27:43 +0100

    Add new test case to numberToString function

--- convert-number-to-string/index.spec.ts
+++ convert-number-to-string/index.spec.ts
@@ -5,6 +5,8 @@ import {
 describe('numberToString', () => {
   it('should convert number to string', () => {
     const converted = numberToString(21)
+    const converted2 = numberToString(99999)
     expect(converted).toEqual('21')
+    expect(converted2).toEqual('99999')
   })
 })
