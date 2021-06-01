b55c590909cf97d3c1a38e8c9a50028cd6cf2dad
AdamCsordas21 <adamcsordas@hotmail.com>
2021-06-01 19:25:54 +0100

    Add new test case to digitize function

--- convert-number-to-reversed-array-of-digits/index.spec.ts
+++ convert-number-to-reversed-array-of-digits/index.spec.ts
@@ -5,6 +5,8 @@ import {
 describe('digitize', () => {
   it('should convert number too a reversed array of digits', () => {
     const digitized = digitize(35231)
+    const digitized2 = digitize(9764197654)
     expect(digitized).toEqual([1, 3, 2, 5, 3])
+    expect(digitized2).toEqual([4, 5, 6, 7, 9, 1, 4, 6, 7, 9])
   })
 })
